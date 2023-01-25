d7fc95e5b75534fdd368fc09c7d45eb054766325
Tiago Castro <tiago211287@gmail.com>
2023-01-25 16:02:48 -0500

    User can choose custom columns3.

--- R/row1.R
+++ R/row1.R
@@ -3,9 +3,14 @@
 #' @export
 #'
 row1 <- function(x, add_columns) {
-  x <- x %>% select(c("v_gene", "j_gene", "c_gene", "cdr3", "cdr3_nt", "chain", "raw_clonotype_id", "barcode", add_columns = add_columns))
+  selected <- c("v_gene", "j_gene", "c_gene", "cdr3", "cdr3_nt", "chain", "raw_clonotype_id", "barcode")
+  if(!is.null(add_columns)){
+    selected <- c(selected, add_columns)
+    }
+  x <- x %>% select(selected)
   x[["chain"]] <- make.unique(x[["chain"]])
   x[["barcode"]] <- make.unique(x[["barcode"]])
+
   x %>%
     mutate(rn = 1) %>%
     pivot_wider(names_from = "chain", values_from = selected) %>%
